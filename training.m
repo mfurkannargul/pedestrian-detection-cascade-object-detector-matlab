56ae04e8db2d6cf5a546171ef4768e8b243e5a56
Muhammet Furkan Nargül <furkannargul98@gmail.com>
2020-08-02 22:57:23 +0300

    Update training.m

--- training.m
+++ training.m
@@ -1,7 +1,7 @@
 positive_ins = pos_roi_mit;
-pos_dir = fullfile('C:\Users\furka\Desktop\Internship - Signalton Technology\MATLAB\Cascade Classifier\positive');
+pos_dir = fullfile('positive');     % the location of folder of positive images
 addpath(pos_dir);
-neg_dir = fullfile('C:\Users\furka\Desktop\Internship - Signalton Technology\MATLAB\Cascade Classifier\negative');
+neg_dir = fullfile('negative');     % the location of folder of negative images
 
 trainCascadeObjectDetector('trained_model_10_0.1.xml', positive_ins,neg_dir, ...
     'NumCascadeStage', 10,'FalseAlarmRate',0.1);
