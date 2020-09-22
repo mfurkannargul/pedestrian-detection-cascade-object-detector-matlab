b9528d12ca806b12148aa2e79ac34a3464c8c60f
mfurkannargul <furkannargul98@gmail.com>
2020-09-22 13:53:26 +0300

    Update training.m

--- training.m
+++ training.m
@@ -3,5 +3,5 @@ pos_dir = fullfile('positive');     % the location of folder of positive images
 addpath(pos_dir);
 neg_dir = fullfile('negative');     % the location of folder of negative images
 
-trainCascadeObjectDetector('trained_model_91_3205.xml', positive_ins,neg_dir, ...
-    'NumCascadeStage', 7,'FalseAlarmRate',0.15);
+trainCascadeObjectDetector('trained_model_91_3205.xml', positive_ins, neg_dir, ...
+    'NumCascadeStage',7,'FalseAlarmRate',0.15);
